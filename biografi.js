040edfcccfc516968789f960cd97c5ba536b766a
44120N <106742333+44120N@users.noreply.github.com>
2023-04-26 11:24:39 +0700

    Update biografi.js
    Signed-off-by: 44120N <106742333+44120N@users.noreply.github.com>

--- biografi.js
+++ biografi.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $('body').click(function(event) {
-    event.preventDefault();
     var target = $(this).attr('href');
     $('html, body').animate({
       scrollTop: $(target).offset().top - 120
